0f4197ef60b49a1e0d79949443962d9e94f8d080
pancake <pancake@nopcode.org>
2021-06-17 17:54:16 +0200

    fixes

--- r2env/package_manager.py
+++ r2env/package_manager.py
@@ -98,7 +98,8 @@ class PackageManager:
         extra_flags = ""
         if os.path.isfile("/default.prop"):
             extra_flags = " --with-compiler=termux"
-        cmd = "(cd {0} && rm -rf shlr/capstone && ./configure " + extra_flags  + " --with-rpath --prefix={1} 2>&1 && make -j4 2>&1" \
+        cmd = "(cd {0} && rm -rf shlr/capstone && ./configure " + extra_flags + \
+              " --with-rpath --prefix={1} 2>&1 && make -j4 2>&1" \
               "&& make install) > {2}".format(source_path, dst_path, logfile)
         return os.system(cmd) == 0
 
