3613127925d4b6f8e57e2c090b8ffc433a737fb4
pancake <pancake@nowsecure.com>
2021-06-17 19:05:00 +0200

    Fix more command parsing issues (#12)

--- r2env/repl.py
+++ r2env/repl.py
@@ -16,7 +16,8 @@ config             - display current .r2env settings
 install   [pkg]    - build and install given package. Use --meson to use it as the build system.
 uninstall [pkg]    - remove selected package
 use [pkg]          - use r2 package defined. pkg should be a release version or latest.
-version            - show the current Radare version and its origin
+path               - show path of current r2 in use
+version            - show version of r2env
 versions           - List all Radare versions installed
 list               - list all Radare packages available to r2env
 shell              - open a new shell with PATH env var set
@@ -28,11 +29,16 @@ def show_help():
     print_console(HELP_MESSAGE)
 
 
+def show_version():
+    print_console(R2Env().version())
+
+
 actions_with_argument = ["install", "uninstall", "use"]
 actions_with_arguments = ["shell"]
 actions = {
     "init": R2Env().init,
-    "version": R2Env().get_r2_path,
+    "version": show_version,
+    "path": R2Env().get_r2_path,
     "config": R2Env().show_config,
     "list": R2Env().list_available_packages,
     "install": R2Env().install,
@@ -45,7 +51,9 @@ actions = {
 
 
 def run_action(action, args, use_meson):
-    if action not in actions:
+    if action == "":
+        show_help()
+    elif action not in actions:
         print_console("[X] Action not found", ERROR)
     elif action in actions_with_arguments:
         actions[action](" ".join(args))
@@ -75,8 +83,8 @@ def main():
     parser.add_argument("action", help="run specified action. (Run r2env help for more information)",
                         action="store", default=["help"])
     parser.add_argument('args', metavar='args', nargs='*', type=str, help='Specified arguments')
-    parser.add_argument("-v", "--version", help="Show r2env version", action="store_true")
-    parser.add_argument("-m", "--meson", help="Use meson as your build system.", action="store_true")
+    parser.add_argument("-v", "--version", dest="version", help="Show r2env version", action="store_true")
+    parser.add_argument("-m", "--meson", dest="meson", help="Use meson as your build system.", action="store_true")
     args = parser.parse_args()
     if args.version:
         print_console(R2Env().version())
