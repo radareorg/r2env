a455d306b428b57a52b84e22073733e39ed428f1
pancake <pancake@nowsecure.com>
2021-06-17 18:12:47 +0200

    Fix build and shell for termux (#8)

--- r2env/package_manager.py
+++ r2env/package_manager.py
@@ -94,9 +94,13 @@ class PackageManager:
     def _build_using_acr(source_path, dst_path, logfile):
         """Only works in Unix systems"""
         exit_if_not_exists(['make'])
-        print_console("[-] Building package using acr ...")
-        cmd = "(cd {0} && rm -rf shlr/capstone && ./configure --with-rpath --prefix={1} 2>&1 && make -j4 2>&1" \
-              "&& make install) > {2}".format(source_path, dst_path, logfile)
+        print_console("[-] Building package using acr for Termux...")
+        extra_flags = ""
+        if os.path.isfile("/default.prop"):
+            extra_flags = " --with-compiler=termux"
+        cmd = "(cd {0} && rm -rf shlr/capstone && ./configure {1}" \
+              " --with-rpath --prefix={2} 2>&1 && make -j4 2>&1" \
+              "&& make install) > {3}".format(source_path, extra_flags, dst_path, logfile)
         return os.system(cmd) == 0
 
     @staticmethod
