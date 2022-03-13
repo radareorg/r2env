1b81a4f99ea53a941435da366b125fb990b5f27b
Murphy <me@0xmurphy.me>
2022-03-13 19:05:31 +0100

    Add tests to package manager

--- r2env/test/test_package.py
+++ r2env/test/test_package.py
@@ -1,4 +1,6 @@
 import os
+from unittest.mock import MagicMock
+from urllib.error import URLError
 
 from doublex import assert_that
 from hamcrest import equal_to
@@ -167,3 +169,229 @@ class TestPackageManager(unittest.TestCase):
         package = "radare2@2.0.0"
         with self.assertRaises(PackageManagerException):
             self.package_manager.uninstall_package(package, False)
+
+    def test_get_pkgname_success(self):
+        target_pkg = "radare2-5.3.1-w64.zip"
+        target_os = "w64"
+        target_version = "5.3.1"
+        packages_mock = {"radare2": {
+            "versions": [
+            {
+                "id": "5.3.0",
+                "packages": {
+                  "w64": "radare2-5.3.0-w64.zip",
+                }
+            },
+            {
+                "id": target_version,
+                "packages": {
+                  target_os: target_pkg,
+                  "mac_x64": "radare2-5.3.1.pkg",
+                }
+            }]
+        }}
+        self.package_manager._packages = packages_mock
+        assert_that(self.package_manager._get_pkgname("radare2", target_version, target_os), equal_to(target_pkg))
+
+    def test_get_pkgname_failed(self):
+        target_os = "w64"
+        target_version = "5.3.1"
+        packages_mock = {"radare2": {
+            "versions": [
+            {
+                "id": "5.3.0",
+                "packages": {
+                  "w64": "radare2-5.3.0-w64.zip",
+                }
+            },
+        ]
+        }}
+        self.package_manager._packages = packages_mock
+        assert_that(self.package_manager._get_pkgname("radare2", target_version, target_os), equal_to(None))
+
+    @patch("r2env.package_manager.PackageManager._get_pkgname")
+    def test_get_disturl_success(self, mock_getpkg):
+        target_pkg = "radare2-5.3.1-w64.zip"
+        version = "5.2.0"
+        mock_getpkg.return_value = target_pkg
+        dist_url = self.package_manager._get_disturl("mock_profile", version, "mock_cos")
+        expected_url = "/".join([self.package_manager.DOWNLOAD_URL, version, target_pkg])
+        assert_that(dist_url, equal_to(expected_url))
+
+    @patch("r2env.package_manager.PackageManager._get_pkgname")
+    def test_get_disturl_failed(self, mock_getpkg):
+        target_pkg = None
+        mock_getpkg.return_value = target_pkg
+        dist_url = self.package_manager._get_disturl("mock_profile", "mock_version", "mock_cos")
+        assert_that(dist_url, equal_to(None))
+
+    @patch("r2env.package_manager.wget")
+    @patch("r2env.package_manager.PackageManager._get_pkgname")
+    @patch("r2env.package_manager.PackageManager._get_disturl")
+    @patch("r2env.package_manager.host_distname")
+    def test_install_from_dist_wget_is_called(self, mock_host_distname,
+                                              mock__get_disturl,
+                                              mock__get_pkgname,
+                                              mock_wget):
+        patch("r2env.package_manager.os.system").start()
+        pkgname = "radare2-5.3.1-w64.zip"
+        disturl = "http://example.org"
+        pkgfile = "/".join([self.package_manager._r2env_path, "src", pkgname])
+        mock__get_pkgname.return_value = pkgname
+        mock__get_disturl.return_value = disturl
+        mock_host_distname.return_value = "mac_arm64"
+        profile = "radare2"
+        version = "5.6.0"
+        self.package_manager._install_from_dist(profile, version)
+        mock_wget.download.assert_called_once_with(disturl, pkgfile)
+
+    @patch("r2env.package_manager.wget")
+    def test_install_from_dist_wget_error_url(self, mock_wget):
+        pkgname_mock = patch("r2env.package_manager.PackageManager._get_pkgname").start()
+        get_disturl_mock = patch("r2env.package_manager.PackageManager._get_disturl").start()
+        pkgname_mock.return_value = "radare2-5.3.1-w64.zip"
+        get_disturl_mock.return_value = "http://example.org"
+        mock_wget.download.side_effect = URLError("error")
+        with self.assertRaises(PackageManagerException):
+            self.package_manager._install_from_dist("profile", "version")
+
+    @patch("r2env.package_manager.wget")
+    def test_install_from_dist_wget_error_connection(self, mock_wget):
+        pkgname_mock = patch("r2env.package_manager.PackageManager._get_pkgname").start()
+        get_disturl_mock = patch("r2env.package_manager.PackageManager._get_disturl").start()
+        pkgname_mock.return_value = "radare2-5.3.1-w64.zip"
+        get_disturl_mock.return_value = "http://example.org"
+        mock_wget.download.side_effect = ConnectionResetError("error")
+        with self.assertRaises(PackageManagerException):
+            self.package_manager._install_from_dist("profile", "version")
+
+    @patch("r2env.package_manager.wget")
+    def test_install_from_dist_wget_error_connection(self, mock_wget):
+        pkgname_mock = patch("r2env.package_manager.PackageManager._get_pkgname").start()
+        get_disturl_mock = patch("r2env.package_manager.PackageManager._get_disturl").start()
+        pkgname_mock.return_value = "radare2-5.3.1-w64.zip"
+        get_disturl_mock.return_value = "http://example.org"
+        mock_wget.download.side_effect = ConnectionResetError("error")
+        with self.assertRaises(PackageManagerException):
+            self.package_manager._install_from_dist("profile", "version")
+
+    @patch("r2env.package_manager.os.system")
+    @patch("r2env.package_manager.PackageManager._get_pkgname")
+    @patch("r2env.package_manager.PackageManager._get_disturl")
+    @patch("r2env.package_manager.host_distname")
+    def test_install_from_dist_mac(self, mock_host_distname,
+                                   mock__get_disturl,
+                                   mock__get_pkgname,
+                                   mock_system):
+        pkgname = "radare2-5.3.1-mac.pkg"
+        pkgfile = "/".join([self.package_manager._r2env_path, "src", pkgname])
+        mock__get_pkgname.return_value = pkgname
+        mock__get_disturl.return_value = "http://example.org"
+        mock_host_distname.return_value = "mac_arm64"
+        profile = "radare2"
+        version = "5.6.0"
+        patch("r2env.package_manager.wget.download").start()
+        mock_system.return_value = 0
+        assert_that(self.package_manager._install_from_dist(profile, version), equal_to(True))
+        mock_system.assert_called_once_with(f"sudo installer -pkg {pkgfile} -target /")
+
+    @patch("r2env.package_manager.os.system")
+    @patch("r2env.package_manager.PackageManager._get_pkgname")
+    @patch("r2env.package_manager.PackageManager._get_disturl")
+    @patch("r2env.package_manager.host_distname")
+    def test_install_from_dist_linux_deb(self, mock_host_distname,
+                                   mock__get_disturl,
+                                   mock__get_pkgname,
+                                   mock_system):
+        pkgname = "radare2-5.3.1-linux.deb"
+        pkgfile = "/".join([self.package_manager._r2env_path, "src", pkgname])
+        mock__get_pkgname.return_value = pkgname
+        mock__get_disturl.return_value = "http://example.org"
+        mock_host_distname.return_value = "deb_x64"
+        profile = "radare2"
+        version = "5.6.0"
+        patch("r2env.package_manager.wget.download").start()
+        mock_system.return_value = 0
+        assert_that(self.package_manager._install_from_dist(profile, version), equal_to(True))
+        mock_system.assert_called_once_with(f"sudo dpkg -i {pkgfile}")
+
+    @patch("r2env.package_manager.os.system")
+    @patch("r2env.package_manager.PackageManager._get_pkgname")
+    @patch("r2env.package_manager.PackageManager._get_disturl")
+    @patch("r2env.package_manager.host_distname")
+    def test_install_from_dist_mac_os_not_supported(self, mock_host_distname,
+                                                    mock__get_disturl,
+                                                    mock__get_pkgname,
+                                                    mock_system):
+        mock__get_pkgname.return_value = "radare2-5.3.1-mac.zip"
+        mock__get_disturl.return_value = "http://example.org"
+        mock_host_distname.return_value = "not_existing_so"
+        patch("r2env.package_manager.wget.download").start()
+        with self.assertRaises(PackageManagerException):
+            self.package_manager._install_from_dist("radare2", "5.6.0")
+        mock_system.assert_not_called()
+
+    @patch("r2env.package_manager.PackageManager._uninstall_from_dist")
+    @patch("r2env.package_manager.zipfile.ZipFile")
+    def test_install_windows_package_file_not_found(self, zipfile_mock, uninstall_mock):
+        zipfile_mock.extractall.side_effect = FileNotFoundError()
+        with self.assertRaises(PackageManagerException):
+            self.package_manager._install_windows_package("radare2")
+        uninstall_mock.assert_called_once()
+
+    @patch("r2env.package_manager.os.path.isdir")
+    @patch("r2env.package_manager.os.makedirs")
+    def test_install_windows_package_create_dst_folder(self, makedirs_mock, isdir_mock):
+        patch("r2env.package_manager.shutil.copyfile").start()
+        patch("r2env.package_manager.zipfile").start()
+        isdir_mock.return_value = False
+        patch("r2env.package_manager.shutil.copyfile").start()
+        self.package_manager._install_windows_package("radare2")
+        makedirs_mock.assert_called_once_with(os.path.join(self.package_manager._r2env_path, "bin"), 493)
+
+    @patch("r2env.package_manager.os.path.isdir")
+    def test_install_windows_package_successfully(self, isdir_mock):
+        with patch("r2env.package_manager.zipfile.ZipFile") as mock_ZipFile:
+            mock_zip = MagicMock()
+            mock_zip.return_value = ["1", "2", "3"]
+            mock_ZipFile.return_value = mock_zip
+            mock_copy = patch("r2env.package_manager.shutil.copyfile").start()
+            isdir_mock.return_value = True
+            assert_that(self.package_manager._install_windows_package("radare2"), equal_to(True))
+            mock_copy.assert_called_once()
+
+    @patch("r2env.package_manager.host_distname")
+    def test_uninstall_from_dist_windows(self, mock_distname):
+        mock_distname.return_value = "w64"
+        mock_rmtree = patch("r2env.package_manager.shutil.rmtree").start()
+        dst_folder = os.path.join(self.package_manager._r2env_path, "bin")
+        assert_that(self.package_manager._uninstall_from_dist("radare2", "1.0.0"), equal_to(True))
+        mock_rmtree.assert_called_once_with(dst_folder)
+
+    @patch("r2env.package_manager.host_distname")
+    def test_uninstall_from_dist_mac(self, mock_distname):
+        mock_distname.return_value = "mac_arm64"
+        mock_system = patch("r2env.package_manager.os.system").start()
+        mock_system.side_effect = [0,0,0,0]
+        assert_that(self.package_manager._uninstall_from_dist("radare2", "1.0.0"), equal_to(True))
+
+    @patch("r2env.package_manager.host_distname")
+    def test_uninstall_from_dist_mac_failed(self, mock_distname):
+        mock_distname.return_value = "mac_arm64"
+        mock_system = patch("r2env.package_manager.os.system").start()
+        mock_system.side_effect = [0,1,0,0]
+        assert_that(self.package_manager._uninstall_from_dist("radare2", "1.0.0"), equal_to(False))
+
+    @patch("r2env.package_manager.host_distname")
+    def test_uninstall_from_dist_linux(self, mock_distname):
+        mock_distname.return_value = "deb_x64"
+        mock_system = patch("r2env.package_manager.os.system").start()
+        mock_system.return_value = 0
+        assert_that(self.package_manager._uninstall_from_dist("radare2", "1.0.0"), equal_to(True))
+        mock_system.assert_called_once_with("sudo dpkg -r radare2")
+
+    @patch("r2env.package_manager.host_distname")
+    def test_uninstall_from_dist_os_not_supported(self, mock_distname):
+        mock_distname.return_value = "non_existent"
+        with self.assertRaises(PackageManagerException):
+            self.package_manager._uninstall_from_dist("radare2", "1.0.0")
