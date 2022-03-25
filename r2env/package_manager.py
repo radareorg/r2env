f7c4c8e883134f4cda4693845a3f1a1075ca7698
Murphy <me@0xmurphy.me>
2022-03-25 21:26:27 +0100

    Fixing lints

--- r2env/package_manager.py
+++ r2env/package_manager.py
@@ -191,7 +191,7 @@ class PackageManager:
     def _build_using_acr(source_path, dst_path, logfile, r2env_path):
         """Only works in Unix systems"""
         if host_distname() in "w64":
-            raise PackageManagerException(f"acr is not supported on Windows platform. Use meson instead.")
+            raise PackageManagerException("acr is not supported on Windows platform. Use meson instead.")
         exit_if_not_exists(['make'])
         print_console("[-] Building package using acr...")
         extra_flags = ""
