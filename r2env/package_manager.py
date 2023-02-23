ed6b184c017398933a9234152a888e2b86a376a6
Lazula <26179473+Lazula@users.noreply.github.com>
2023-02-23 02:00:11 -0600

    Fix None in string error from host_distname() on unsupported systems

--- r2env/package_manager.py
+++ r2env/package_manager.py
@@ -190,7 +190,7 @@ class PackageManager:
     @staticmethod
     def _build_using_acr(source_path, dst_path, logfile, r2env_path):
         """Only works in Unix systems"""
-        if host_distname() in "w64":
+        if host_distname() is not None and host_distname() in "w64":
             raise PackageManagerException("acr is not supported on Windows platform. Use meson instead.")
         exit_if_not_exists(['make'])
         print_console("[-] Building package using acr...")
